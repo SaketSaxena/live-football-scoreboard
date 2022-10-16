e31eb2da700036350cfe6c7c49c94f604140014c
SaketSaxena <saket.saxena@posten.no>
2022-10-16 16:11:32 +0200

    Add test cases

--- src/test/StartGame.test.tsx
+++ src/test/StartGame.test.tsx
@@ -1,17 +1,24 @@
 import * as React from "react";
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import StartGame from "../component/StartGame";
 
 describe("StartGame", () => {
   afterEach(cleanup);
-
-  const renderComponent = () => render(<StartGame />);
+  const updateScoreBoard = jest.fn(() => {});
+  const renderComponent = () =>
+    render(<StartGame updateScoreBoard={updateScoreBoard} />);
 
   it("should render start game components ", () => {
     const { getAllByRole, getByRole } = renderComponent();
     expect(getAllByRole("textbox")[0]).toBeInTheDocument();
     expect(getAllByRole("textbox")[1]).toBeInTheDocument();
     expect(getByRole("button")).toBeInTheDocument();
+  });
+
+  it("should be able to click on start new button", () => {
+    const { getByRole } = renderComponent();
+    const button = getByRole("button");
+    fireEvent.click(button);
   });
 });
