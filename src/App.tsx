e9790f27a8f7cf255708d76ca46a81a21e569468
SaketSaxena <saket.saxena@posten.no>
2022-10-16 16:11:24 +0200

    Add logic to add new game to score board list

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,22 @@ import React from "react";
 import StartGame from "./component/StartGame";
 import Container from "@mui/material/Container";
 import { Grid } from "@mui/material";
+import { ScoreBoard } from "./types";
+import { useLocalStorage } from "./hook/localStorage.js";
 
 const App = () => {
+  const [scoreBoard, setScoreBoard] = useLocalStorage("scoreBoard", []);
+
+  const updateScoreBoard = (newScoreBoard: ScoreBoard) => {
+    setScoreBoard([...scoreBoard, newScoreBoard]);
+  };
+
   return (
     <Container maxWidth="lg">
       <h1>Welcome to Live Football Scoreboard</h1>
       <Grid container>
         <Grid item xs={6}>
-          <StartGame />
+          <StartGame updateScoreBoard={updateScoreBoard} />
         </Grid>
         <Grid item xs={6}>
           ...
